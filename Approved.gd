7fec91d584c6ae65c68d71e16f47cfd11b685347
kuatw <98233145+kuatw@users.noreply.github.com>
2023-11-18 14:06:20 -0500

    stamps notebook
    stamper notecover change to notepaperm mouse clicking, stamper dragging, stamper moving , print the stamp on the screen

---
+++ Approved.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+signal mouse_released
+
+var picked_up: bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if picked_up:
+		global_position = get_global_mouse_position()
+		
+	if Input.is_action_just_pressed("RMB"):
+		mouse_released.emit()
+		picked_up = false
+
+
+
+func _on_button_pressed():
+	picked_up = true
+	await mouse_released
+
