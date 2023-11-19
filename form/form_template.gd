9c876e8e7a45f8013485a21ae29dcae182c3171c
AdkinsC0211 <adkinsc0211@gmail.com>
2023-11-19 18:05:39 -0500

    adapted form

--- form/form_template.gd
+++ form/form_template.gd
@@ -5,3 +5,7 @@ var organization = "Place"
 var amount = "$1,000,000"
 var reason = "I want money"
 var details = "Just because..."
+var target_amount = 0
+var largest_amount = 0
+var target_approved = 0
+var plz_fix = false
