746bc01d777436dd3ecc9fdd688201ecc3768276
David McCool <mccoold7@gmail.com>
2023-11-18 20:08:34 -0500

    Added camera panning limits
    Camera cannot escape the top or bottom of the background

--- scripts/camera_move.gd
+++ scripts/camera_move.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("camera_down"):
+	if Input.is_action_pressed("camera_down") and global_position.y <= 660:
 		self.global_position.y += 350 * delta
-	if Input.is_action_pressed("camera_up"):
+	if Input.is_action_pressed("camera_up") and global_position.y >= 330:
 		self.global_position.y -= 350 * delta
