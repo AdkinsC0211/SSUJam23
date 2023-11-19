bad4202a43bcd3d992d91b98cbb220b78ec63740
David McCool <mccoold7@gmail.com>
2023-11-19 13:27:40 -0500

    Switched clampi to clampf
    Used floats to eliminate warnings on the camera clamps

--- scripts/camera_move.gd
+++ scripts/camera_move.gd
@@ -13,6 +13,6 @@ func _process(delta):
 	if Input.is_action_pressed("camera_up") and global_position.y >= 330:
 		self.global_position.y -= 350 * delta
 	if Input.is_action_just_pressed("camera_up"):
-		global_position.y = clampi(global_position.y - 30, 330, 660)
+		global_position.y = clampf(global_position.y - 30.0, 330.0, 660.0)
 	if Input.is_action_just_pressed("camera_down"):
-		global_position.y = clampi(global_position.y + 30, 330, 660)
+		global_position.y = clampf(global_position.y + 30.0, 330.0, 660.0)
