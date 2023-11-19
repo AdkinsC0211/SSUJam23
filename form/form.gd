8163094e9c98025674f7cfadd10fb9fd84735aa3
AdkinsC0211 <adkinsc0211@gmail.com>
2023-11-19 13:50:55 -0500

    fixed up forms

--- form/form.gd
+++ form/form.gd
@@ -1,3 +1,16 @@
 extends "res://scripts/paper_button_version.gd"
 
 var approved = -1
+
+var form_details = Form.new()
+
+func _ready():
+	pass
+
+
+func fill_form(new_form:Form):
+	$Name.text = "Name: " + new_form.alias
+	$Org.text = "Organization / Company: " + new_form.organization
+	$Amnt.text = "Requested Amount: " + new_form.amount
+	$Reason.text = "Reason for Request: " + new_form.reason
+	$Details.text = "Further Details:\n" + new_form.details
---
+++ form/form_template.gd
@@ -0,0 +1,15 @@
+class_name Form extends Resource
+
+var alias = "Tom"
+var organization = "Place"
+var amount = "$1,000,000"
+var reason = "I want money"
+var details = "Just because..."
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
