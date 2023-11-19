3362cb56337535d3209a12b1d54c0fdfc1d32aa8
AdkinsC0211 <adkinsc0211@gmail.com>
2023-11-19 17:28:27 -0500

    stickies

---
+++ scenes/stickies.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var note = preload("res://scenes/stickynote.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_texture_button_pressed():
+	var temp = note.instantiate()
+	temp.held = true
+	get_parent().add_child(temp)
+	$"/root/Singleton".play_sound("sticky up")
---
+++ scenes/stickynote.gd
@@ -0,0 +1,34 @@
+extends Node2D
+var held = false
+var tex1 = preload("res://note1.png")
+var tex2 = preload("res://note2.png")
+var tex3 = preload("res://note3.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	match randi_range(1, 3):
+		1:
+			$TextureButton.texture = tex1
+		2:
+			$TextureButton.texture = tex2
+		3:
+			$TextureButton.texture = tex3
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if held:
+		global_transform.origin = get_global_mouse_position()
+	if Input.is_action_just_pressed("LMB") and held:
+		var highest = null
+		var height_index = 9999999
+		for i in $Area2D.get_overlapping_areas():
+			if i.get_parent().get_index() < height_index:
+				height_index = i.get_parent().get_index()
+				highest = i.get_parent()
+		if highest != null:
+			self.reparent(highest)
+			$"/root/Singleton".play_sound("sticky down")
+			if highest.get("approved")!=null:
+				highest.approved = 0
+			held = false
