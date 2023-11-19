e92f051d7a675583c6ce78b7e577b811c9fd8d9a
kuatw <sammi510429@gmail.com>
2023-11-19 16:09:59 -0500

    book display
    book display over desktop instead of changing scene

--- notepaper.gd
+++ notepaper.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-
+var notepaper
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	notepaper = $/root/desk_root/book_manager/notepaper
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("GoBack"):
 		get_tree().change_scene_to_file("res://scenes/desk.tscn")
+
+
+func _on_button_pressed():
+	notepaper.visible = false
